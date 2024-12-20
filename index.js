44cdd03267667ab31f1d1c64f4f0272396db4b4a
Razzoy <61683221+Razzoy@users.noreply.github.com>
2024-12-20 11:35:47 +0100

    https

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.listen(PORT, () => {
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "tamagotchibackend-production.up.railway.app",
+    origin: "https://tamagotchibackend-production.up.railway.app",
   },
 });
 
